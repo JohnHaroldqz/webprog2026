a5b4bb306db79295154f13c196e479865f46d216
john harold <john@gmail.com>
2026-01-19 14:00:29 +0800

    Used Winston as logger and configured it.
    Tested also a sample error.

--- server/src/controllers/aboutController.ts
+++ server/src/controllers/aboutController.ts
@@ -3,3 +3,7 @@ import type { RequestHandler } from "express"
 export const getAbout:RequestHandler = (req, res) => {
   res.send('This is the about page... working po!')
 }
+
+export const getError:RequestHandler = (req, res) => {
+  res.send('Error ka, negro!')
+}
